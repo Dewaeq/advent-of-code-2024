d77ef2e0aab016ac104a828bd3ca06b7a37e17bf
Dewaeq <54279351+Dewaeq@users.noreply.github.com>
2024-12-02 20:10:23 +0100

    optimized day 2 part 1

--- src/day2.rs
+++ src/day2.rs
@@ -1,60 +1,51 @@
 use aoc_runner_derive::aoc;
 
-fn parse(s: &str) -> i32 {
-    s.bytes().fold(0, |a, c| a * 10 + (c - 30) as i32)
-}
-
 #[aoc(day2, part1)]
 pub fn part1(input: &str) -> i32 {
-    let mut count = 0;
-    'outer: for line in input.lines() {
-        let mut prev = 0;
-        let mut desc = false;
-        let mut add = true;
+    let bytes = input.as_bytes();
 
-        for (i, entry) in line.split(" ").enumerate() {
-            // let level = parse(entry);
-            let level = entry.parse::<i32>().unwrap();
-            if i == 1 {
-                desc = level < prev;
+    let mut count = 0;
+
+    let mut is_safe = true;
+    let mut is_descending = None;
+    let mut level = 0;
+    let mut prev = 0i32;
+
+    for &b in bytes {
+        // space/newline means level is fully read
+        if is_safe && (b == 10 || b == 32) {
+            match (is_descending, prev) {
+                (None, x) if x != 0 => is_descending = Some(level < x),
+                _ => (),
             }
-            if i != 0 {
+            if prev != 0 {
                 let diff = (level - prev).abs();
-                if diff > 3 || diff == 0 || ((level < prev) != desc) {
-                    // continue 'outer;
-                    add = false;
+                if diff > 3 || diff == 0 || is_descending.is_some_and(|desc| (level < prev) != desc)
+                {
+                    is_safe = false;
                 }
             }
 
-            prev = level;
+            (prev, level) = (level, 0);
         }
-        count += add as i32;
+        // newline
+        if b == 10 {
+            count += is_safe as i32;
+            is_safe = true;
+            level = 0;
+            prev = 0;
+            is_descending = None;
+        }
+        // non-digit
+        if b < 48 {
+            continue;
+        }
+
+        level *= 10;
+        level += (b & 0xf) as i32;
     }
 
-    count
-}
-#[aoc(day2, part1, cont)]
-pub fn part1_cont(input: &str) -> i32 {
-    let mut count = 0;
-    'outer: for line in input.lines() {
-        let mut prev = 0;
-        let mut desc = false;
-
-        for (i, entry) in line.split(" ").enumerate() {
-            // let level = parse(entry);
-            let level = entry.parse::<i32>().unwrap();
-            if i == 1 {
-                desc = level < prev;
-            }
-            if i != 0 {
-                let diff = (level - prev).abs();
-                if diff > 3 || diff == 0 || ((level < prev) != desc) {
-                    continue 'outer;
-                }
-            }
-
-            prev = level;
-        }
+    if is_descending.is_some() {
         count += 1;
     }
 
@@ -64,19 +55,4 @@ pub fn part1_cont(input: &str) -> i32 {
 #[aoc(day2, part2)]
 fn part2(input: &str) -> String {
     todo!()
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    /*     #[test]
-    fn part1_example() {
-        assert_eq!(part1(&parse("<EXAMPLE>")), "<RESULT>");
-    }
-
-    #[test]
-    fn part2_example() {
-        assert_eq!(part2(&parse("<EXAMPLE>")), "<RESULT>");
-    } */
 }
