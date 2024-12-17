5a2ee4b379f93a17a3c5f299ce14780ebc1cbc0e
Dewaeq <dewaeq@protonmail.com>
2024-12-17 20:43:07 +0100

    day 17: part two

--- src/day17.rs
+++ src/day17.rs
@@ -1,11 +1,11 @@
 use aoc_runner_derive::aoc;
 
 struct Program {
-    pub register_a: i32,
-    pub register_b: i32,
-    pub register_c: i32,
-    pub commands: Vec<u8>,
-    pub output: Vec<i32>,
+    a: u64,
+    b: u64,
+    c: u64,
+    commands: Vec<u8>,
+    output: Vec<u8>,
 }
 
 impl Program {
@@ -18,11 +18,14 @@ impl Program {
             let operand = instruction.operand(self.commands[pointer + 1], self);
 
             instruction.execute(operand, self, &mut pointer);
-
-            if instruction != Instruction::Jnz {
-                pointer += 2;
-            }
         }
+    }
+
+    fn reset(&mut self) {
+        self.a = 0;
+        self.b = 0;
+        self.c = 0;
+        self.output.clear();
     }
 }
 
@@ -40,58 +43,48 @@ enum Instruction {
 }
 
 impl Instruction {
-    fn operand(&self, index: u8, program: &Program) -> i32 {
+    fn operand(&self, index: u8, program: &Program) -> u64 {
         match self {
             Instruction::Bxl | Instruction::Jnz => index as _,
             _ => match index {
                 x @ 0..=3 => x as _,
-                4 => program.register_a,
-                5 => program.register_b,
-                6 => program.register_c,
+                4 => program.a,
+                5 => program.b,
+                6 => program.c,
                 _ => unreachable!(),
             },
         }
     }
 
-    fn execute(self, operand: i32, program: &mut Program, pointer: &mut usize) {
+    fn execute(self, operand: u64, program: &mut Program, pointer: &mut usize) {
         match self {
-            Instruction::Adv => {
-                program.register_a = program.register_a / 2i32.pow(operand as _);
-            }
-            Instruction::Bxl => {
-                program.register_b = operand ^ program.register_b;
-            }
-            Instruction::Bst => {
-                program.register_b = operand % 8;
-            }
+            Instruction::Adv => program.a = program.a / 2u64.pow(operand as _),
+            Instruction::Bxl => program.b = operand ^ program.b,
+            Instruction::Bst => program.b = operand % 8,
             Instruction::Jnz => {
-                if program.register_a != 0 {
+                if program.a != 0 {
                     *pointer = operand as _;
                 } else {
                     *pointer += 2;
                 }
             }
-            Instruction::Bxc => {
-                program.register_b = program.register_b ^ program.register_c;
-            }
-            Instruction::Out => {
-                program.output.push(operand % 8);
-            }
-            Instruction::Bdv => {
-                program.register_b = program.register_a / 2i32.pow(operand as _);
-            }
-            Instruction::Cdv => {
-                program.register_c = program.register_a / 2i32.pow(operand as _);
-            }
+            Instruction::Bxc => program.b = program.b ^ program.c,
+            Instruction::Out => program.output.push((operand % 8) as _),
+            Instruction::Bdv => program.b = program.a / 2u64.pow(operand as _),
+            Instruction::Cdv => program.c = program.a / 2u64.pow(operand as _),
+        }
+
+        if self != Instruction::Jnz {
+            *pointer += 2;
         }
     }
 }
 
 fn parse(input: &str) -> Program {
     let mut lines = input.lines();
-    let a = lines.next().unwrap()[12..].parse::<i32>().unwrap();
-    let c = lines.next().unwrap()[12..].parse::<i32>().unwrap();
-    let b = lines.next().unwrap()[12..].parse::<i32>().unwrap();
+    let a = lines.next().unwrap()[12..].parse::<u64>().unwrap();
+    let c = lines.next().unwrap()[12..].parse::<u64>().unwrap();
+    let b = lines.next().unwrap()[12..].parse::<u64>().unwrap();
     lines.next().unwrap();
 
     let commands = lines.next().unwrap()[9..]
@@ -100,9 +93,9 @@ fn parse(input: &str) -> Program {
         .collect::<Vec<_>>();
 
     Program {
-        register_a: a,
-        register_b: b,
-        register_c: c,
+        a,
+        b,
+        c,
         commands,
         output: vec![],
     }
@@ -123,30 +116,63 @@ fn part1(input: &str) -> String {
 }
 
 #[aoc(day17, part2)]
-fn part2(input: &str) -> i32 {
+fn part2(input: &str) -> u64 {
+    // 2,4,1,5,7,5,0,3,1,6,4,3,5,5,3,0
+    //
+    // (2 4) => b = a % 8
+    // (1 5) => b = b ^ 5
+    // (7 5) => c = a // 2**b
+    // (0 3) => a = a // 2**3
+    // (1 6) => b = b ^ 6
+    // (4 3) => b = b ^ c
+    // (5 5) => output.push(b % 8)	(*)
+    // (3 0) => repeat if a != 0
+
+    // so:
+    // b = a % 8
+    // b = b ^ 5
+    // c = a >> b
+    // a = a >> 3
+    // b = b ^ 6
+    // b = b ^ c
+    // out(b % 8)
+    // repeat if a != 0
+    //
+    // which simplifies to:
+    // while a != 0 {
+    //     out(f(a) % 8)
+    //     a = a >> 3
+    // }
+    //
+    // so, the plan is to iteratively find each 3 bit chunk of A
+
     let mut program = parse(input);
-    let b = program.register_b;
-    let c = program.register_c;
-    let commands = program.commands.clone();
+    let target_output = program.commands.clone();
+    let n = target_output.len();
 
-    let target_output = commands.iter().map(|&c| c as i32).collect::<Vec<_>>();
-    let mut a = 0;
+    let mut parts = vec![0];
 
-    loop {
-        program.register_a = a;
-        program.register_b = b;
-        program.register_c = c;
-        program.commands = commands.clone();
-        program.output.clear();
+    for i in (0..n).rev() {
+        let mut new_parts = vec![];
 
-        program.run();
+        for part in &parts {
+            for next_part in 0..8 {
+                let a = (part << 3) + next_part;
+                program.a = a;
+                program.run();
 
-        if program.output == target_output {
-            return a;
+                if program.output == target_output[i..] {
+                    new_parts.push(a);
+                }
+
+                program.reset();
+            }
         }
 
-        a += 1;
+        parts = new_parts;
     }
+
+    *parts.iter().min().unwrap()
 }
 
 #[cfg(test)]
